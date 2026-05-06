1d3d73d40accf80bb44842ac99dc725f8f1e33c3
anthony <lukenganga443@gmail.com>
2026-05-06 12:57:22 +0300

    Add initial HTML, CSS, and JavaScript files for RecipeVault

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("RecipeVault loaded 🚀");
+
+const links = document.querySelectorAll("nav a");
+
+links.forEach((link) => {
+  if (link.href === window.location.href) {
+    link.classList.add("active");
+  }
+});
