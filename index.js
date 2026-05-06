373077174f343337837ff8c0ef622e51df6967c2
anthony <lukenganga443@gmail.com>
2026-05-06 14:39:44 +0300

    Refactor index.html and index.js for improved content clarity; update styles.css for enhanced layout and styling

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 function reloadPage() {
   document.body.style.opacity = "0.2";
-
-  setTimeout(() => {
-    location.reload();
-  }, 200);
+  setTimeout(() => location.reload(), 200);
 }
-window.addEventListener("load", () => {
-  document.body.style.opacity = "1";
-});
+async function loadTrending() {
+  const container = document.getElementById("trendingResults");
+
+  const res = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/search.php?s=",
+  );
+
+  const data = await res.json();
+
+  container.innerHTML = "";
+
+  data.meals.slice(0, 8).forEach((meal) => {
+    container.innerHTML += `
+      <div class="trend-card" onclick="openMeal('${meal.idMeal}')">
+        <img src="${meal.strMealThumb}">
+        <p style="padding:10px">${meal.strMeal}</p>
+      </div>
+    `;
+  });
+}
+function openMeal(id) {
+  localStorage.setItem("selectedMeal", id);
+  window.location.href = "recipe.html";
+}
+loadTrending();
