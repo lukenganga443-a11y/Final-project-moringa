1d5355aca5c5389612b6866a76aeb448ed430041
anthony <lukenganga443@gmail.com>
2026-05-07 17:10:36 +0300

    Enhance recipe details layout and styling; improve navigation, loading messages, and ingredient presentation

--- recipe.js
+++ recipe.js
@@ -1,19 +1,62 @@
-function goHome(){
+function goHome() {
+  window.location.href = "index.html";
+}
+
+const mealId = localStorage.getItem("selectedMeal");
+
+/* LOAD RECIPE */
+async function loadMeal() {
+  const container = document.getElementById("mealDetails");
+
+  try {
+    const res = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`,
+    );
+
+    const data = await res.json();
+
+    const meal = data.meals[0];
+
+    let ingredients = "";
+
+    for (let i = 1; i <= 20; i++) {
+      const ingredient = meal[`strIngredient${i}`];
+
+      const measure = meal[`strMeasure${i}`];
+
+      if (ingredient && ingredient.trim() !== "") {
+        ingredients += `
+          <li>
+            ${measure} ${ingredient}
+          </li>
+        `;
       }
     }
 
     container.innerHTML = `
+
       <div class="recipe-container">
 
-        <img src="${meal.strMealThumb}">
+        <img
+          src="${meal.strMealThumb}"
+          alt="${meal.strMeal}"
+        >
 
         <div class="recipe-content">
 
           <h1>${meal.strMeal}</h1>
 
-          <p><strong>Category:</strong> ${meal.strCategory}</p>
+          <div class="recipe-meta">
 
-          <p><strong>Cuisine:</strong> ${meal.strArea}</p>
+            <div class="meta-box">
+              Category: ${meal.strCategory}
+            </div>
+
+            <div class="meta-box">
+              Cuisine: ${meal.strArea}
+            </div>
+
+          </div>
 
           <h2>Instructions</h2>
 
@@ -21,29 +64,41 @@ function goHome(){
             ${meal.strInstructions}
           </p>
 
-          <h2>Ingredients</h2>
+          <div class="ingredients">
 
-          <ul>
-            ${ingredients}
-          </ul>
+            <h2>Ingredients</h2>
 
-          <a
-            href="${meal.strYoutube}"
-            target="_blank"
-            class="video-btn"
-          >
-            Watch Tutorial
-          </a>
+            <ul>
+              ${ingredients}
+            </ul>
+
+          </div>
+
+          ${
+            meal.strYoutube
+              ? `
+            <a
+              href="${meal.strYoutube}"
+              target="_blank"
+              class="video-btn"
+            >
+              Watch Tutorial
+            </a>
+            `
+              : ""
+          }
 
         </div>
 
       </div>
+
     `;
-
-  }catch(error){
-
-    container.innerHTML =
-    `<p>Failed to load recipe.</p>`;
+  } catch (error) {
+    container.innerHTML = `
+      <p class="loading">
+        Failed to load recipe.
+      </p>
+    `;
   }
 }
 
